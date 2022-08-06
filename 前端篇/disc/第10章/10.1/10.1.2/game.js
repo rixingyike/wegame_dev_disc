056a5c91585c6f4dcb5777082f608aee6047bd05
榨知机 <9830131@qq.com>
2022-08-06 08:40:24 +0800

    2022-08-06_08:40

--- 前端篇/disc/第10章/10.1/10.1.2/game.js
+++ 前端篇/disc/第10章/10.1/10.1.2/game.js
@@ -1,3 +1,10 @@
+/**
+ * 《微信小游戏开发：前端篇》随课源码
+ * @author LIYI <9830131@qq.com>
+ * 公众号/视频号「网络榨知机」作者，腾讯云最具价值专家（TVP）、微信学堂《小程序性能优化实践》等课程讲师、《小程序从0到1》《微信小游戏开发》等图书作者。
+ * 学编程就像登山，一步一个台阶，没有人学不会。
+*/
+
 // JS：disc\第10章\10.1\10.1.2\game.js
 import "src/consts.js" // 引入常量
 import audioManager from "src/managers/audio_manager.js" // 音频管理者单例
